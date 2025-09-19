b51c436473afdefb189a44679b8d848c2acc2084
Mariam Ayman <mariam.meligy02@gmail.com>
2025-09-19 17:00:07 +0300

    Updated portfolio

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault();
     alert('Thank you for your message!');
 });
+
+// in script.js
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
